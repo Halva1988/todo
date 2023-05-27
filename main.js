12f173a97868b7b44e4340d182c8a0cf55bb3495
khalaimaleksandr <Khalaimaleksandr@gmail.com>
2023-05-28 00:25:19 +0300

    rewrote to functional expressions, added localstorage, added render, element removal

--- main.js
+++ main.js
@@ -1,37 +1,134 @@
-class Todo {
-  constructor(desc, container ='.todo__list') {
-    this.desc = desc;
-    this.constainer = document.querySelector(container)
+function createTodo(description, storage) {
+  const container = document.querySelector('.todo__list');
 
-    this.createTodo()
+  if (description) {
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todo__item');
+
+    const checkbox = createCheckbox(storage);
+    const label = createLabel(storage);
+    const descriptionPara = createDescription(description);
+    const removeImg = createRemoveImage();
+
+    todoItem.appendChild(checkbox);
+    todoItem.appendChild(label);
+    label.appendChild(descriptionPara);
+    todoItem.appendChild(removeImg);
+    container.appendChild(todoItem);
+
+    storage.push({
+      checkbox: false,
+      description: description,
+      id: storage.length
+    });
   }
 
-  createTodo() {
-    if (this.desc) {
-      this.constainer.insertAdjacentHTML('beforeend', `
-      <div class="todo__item">
-        <input class="todo__done" type="checkbox">
-        <p class="todo__description">${this.desc}</p>
-        <img class="todo__remove" src="./asset/img/basket.png" alt="basket">
-       </div>
-      `);
-
-      const remove = document.querySelector('.todo__remove');
-
-      remove.addEventListener('click', event => {
-        event.target.parentNode.remove()
-      })
-    }
-
-  }
+  setLocalStorage(storage);
 }
 
-const form = document.querySelector('.add__todo');
+function createCheckbox(storage) {
+  const checkbox = document.createElement('input');
+  checkbox.classList.add('todo__done');
+  checkbox.type = 'checkbox';
+  checkbox.id = storage.length;
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  const desc = document.querySelector('.add__input').value;
-  const item = new Todo(desc);
+  return checkbox;
+}
+
+function createLabel(storage) {
+  const label = document.createElement('label');
+  label.setAttribute('for', storage.length);
+
+  return label;
+}
+
+function createDescription(description) {
+  const descriptionPara = document.createElement('p');
+  descriptionPara.classList.add('todo__description');
+  descriptionPara.textContent = description;
+
+  return descriptionPara;
+}
+
+function createRemoveImage() {
+  const removeImg = document.createElement('img');
+  removeImg.classList.add('todo__remove');
+  removeImg.src = './asset/img/basket.png';
+  removeImg.alt = 'basket';
+
+  return removeImg;
+}
+
+function setLocalStorage(data) {
+  return localStorage.setItem('todoData', JSON.stringify(data))
+}
+
+function getLocalstorage() {
+  return JSON.parse(localStorage.getItem('todoData')) || [];
+}
+
+function renderOldTodo() {
+  const list = document.querySelector('.todo__list');
+  list.innerHTML = '';
+  const storage = getLocalstorage();
+
+  storage.forEach(({ checkbox, description }, index) => {
+    const todoItem = document.createElement('div');
+    todoItem.classList.add('todo__item');
+
+    const checkboxEl = document.createElement('input');
+    checkboxEl.classList.add('todo__done');
+    checkboxEl.type = 'checkbox';
+    checkboxEl.id = index;
+    checkboxEl.checked = checkbox;
+
+    const label = document.createElement('label');
+    label.setAttribute('for', index);
+
+    const descriptionPara = createDescription(description);
+    const removeImg = createRemoveImage();
+
+    label.appendChild(descriptionPara);
+    todoItem.appendChild(checkboxEl);
+    todoItem.appendChild(label);
+    todoItem.appendChild(removeImg);
+
+    list.appendChild(todoItem);
+  });
+
+  list.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('todo__done')) {
+      const index = target.id;
+      const storage = getLocalstorage();
+      storage[index].checkbox = target.checked;
+
+      setLocalStorage(storage);
+    } else if (target.classList.contains('todo__remove')) {
+      const index = Array.from(target.parentNode.parentNode.children).indexOf(target.parentNode);
+
+      target.parentNode.remove();
+
+      const storage = getLocalstorage();
+
+      storage.splice(index, 1);
+      setLocalStorage(storage);
+    }
+  });
+}
+
+function addNewTodo(event) {
+  event.preventDefault();
+  const description = document.querySelector('.add__input').value;
+  const storage = getLocalstorage();
+
+  createTodo(description, storage);
   event.target.reset();
-})
+}
 
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.add__todo');
+  form.addEventListener('submit', addNewTodo);
+
+  renderOldTodo();
+})
